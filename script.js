ecacc9515f190e506cca9d455074126658152a71
Lorenzothe <140451743+Lorenzothe@users.noreply.github.com>
2025-07-08 08:49:58 -0300

    a

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 function toggleMenu() {
     const menu = document.getElementById('navMenu');
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
-}
-
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(section => section.style.display = 'none');
-    document.getElementById(sectionId).style.display = 'block';
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
